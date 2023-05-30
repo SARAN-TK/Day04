62196e5d7f1a7d1e06d561180272344e84f8d8b9
SARAN-TK <132889778+SARAN-TK@users.noreply.github.com>
2023-05-30 12:51:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './components/User';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <br/> <br/><br/> <br/><User/>
+    </div>
+  );
+}
+
+export default App;
--- User.jsx
+++ User.jsx
@@ -1,40 +1,58 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
-import { Dialog } from 'primereact/dialog';
+import { Box, Button, Container, Typography } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+
 const User = () => {
-    const [image, setimage] = useState("")
+  const [image, setImage] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type.substring(0, 5) === 'image') {
+      setImage(file);
+    } else {
+      setImage(null);
+    }
+  };
+
   return (
-    <div className='profile_img text-center p-4'>
-     <div className="flex flex-column justify-content-center align-items-center">
-
-    <InputText 
-    type='file'
-    
-    
-    accept='/image/*'
-    onChange={(event)=>{
-        const file=event.target.files[0];
-        if(file && file.type.substring(0,5)==="image"){
-            setimage(file);
-
-        }else{
-            setimage(null)
-        }
-    }}
-    
-    
-    />
 
 
-     </div>
+    
+   
+    <div className="profile_img text-center p-4">
+      <div className="flex flex-column justify-content-center align-items-center">
+        <InputText type="file" accept="image/*" onChange={handleFileChange} />
+        
+        <br /><br /> <br />
+        <Stack direction="row" spacing={2}>
+         <Avatar sx={{ width:50 , height:50 , padding:"60px" ,  marginLeft:"800px" }}  alt="Remy Sharp" src="C:\project\profile.jpg" /> 
+    </Stack>
+
+        <br /> 
+        <br /> <br /><br /><br /><br /> <br /><br /><Typography align='center' sx={{flexGrow:1}}><h2>USER DETAILS</h2></Typography>
+         <Box sx={{bgcolor:"",height:'13vh',weidth:'60vh'}}> 
+         <Container fixed>
+            <div className="details"/>
+            <Typography paddingRight={"500px"}>NAME  :</Typography>
+            <Typography paddingRight={"500px"}>PLACE :</Typography>
+            <Typography paddingRight={"500px"}> AGE   :</Typography>
+            <Typography paddingRight={"500px"}>EMAIL :</Typography>
+            <Typography paddingRight={"500px"}>PH NO :</Typography>
+            
+         </Container>
+         </Box>
+         <br /> <br /> <br /><Button variant="contained" color='success'>EDIT</Button>
+
+         
+        
+      </div>
     </div>
-  )
-}
+  );
+};
+ 
 
-export default User
-
-
-
-
+export default User;
 
 
